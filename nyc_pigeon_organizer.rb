8cc9b5ddbbd56a0d6829856a9ed54ab44db6c994
Robert Peck <rspeckpfs@gmail.com>
2017-09-10 16:16:25 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,17 +1,41 @@
 require 'pry'
 
+#||= operator is awesome! It means 'a || a = b', ie, if a evaluates to false, set a = b.
+#so  if !output)_hash[name].include?(data_type)
+#       output_hash[name][data_type] = []
+#    end
+#can be rewritten as:
+#    output_hash[name][data_type] ||= []
+
+
+#This was my original code, I rewrote it below with the ||= operator. So much simpler and easier to read!
+# def nyc_pigeon_organizer(data)
+#   # write your code here!
+#   output_hash = {}
+#   data.each {|data_type, data_hash| #iterate through everything. blah blah, used binding.pry to make sure each level was correct.
+#       data_hash.each {|key, value_array| #I hate these parameter names. Not very descriptive
+#         value_array.each {|name|
+#           if !output_hash.include?(name) #if no name_hash in the output hash, make one
+#             output_hash[name] = {}
+#           end
+#           if !output_hash[name].include?(data_type) #if the type of data hasnt been added, add an empty array to add values to
+#             output_hash[name][data_type] = []
+#           end #supposing all the previous works, shovel values onto each data_type array.
+#             output_hash[name][data_type] << key.to_s #turn symbol into string to make the output pretty.
+#         }
+#       }
+#   }
+#   output_hash
+# end
+
 def nyc_pigeon_organizer(data)
   # write your code here!
   output_hash = {}
   data.each {|data_type, data_hash| #iterate through everything. blah blah, used binding.pry to make sure each level was correct.
       data_hash.each {|key, value_array| #I hate these parameter names. Not very descriptive
         value_array.each {|name|
-          if !output_hash.include?(name) #if no name_hash in the output hash, make one
-            output_hash[name] = {}
-          end
-          if !output_hash[name].include?(data_type) #if the type of data hasnt been added, add an empty array to add values to
-            output_hash[name][data_type] = []
-          end #supposing all the previous works, shovel values onto each data_type array.
+            output_hash[name] ||= {}
+            output_hash[name][data_type] ||= []
             output_hash[name][data_type] << key.to_s #turn symbol into string to make the output pretty.
         }
       }
