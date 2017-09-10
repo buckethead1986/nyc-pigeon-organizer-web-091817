7e2a6435862d0d162af14e7a1675227221e4ee0f
Robert Peck <rspeckpfs@gmail.com>
2017-09-10 16:02:19 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,23 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
   # write your code here!
+  output_hash = {}
+  data.each {|data_type, data_value| #iterate through everything. blah blah, used binding.pry to make sure each level was correct.
+      data_value.each {|key, value_array|
+        value_array.each {|name|
+          if !output_hash.include?(name) #if no name_hash in the output hash, make one
+            output_hash[name] = {}
+          end
+          if !output_hash[name].include?(data_type) #if the type of data hasnt been added, add an empty array to add values to
+            output_hash[name][data_type] = []
+          end #supposing all the previous works, shovel values onto each data_type array.
+            output_hash[name][data_type] << key.to_s #turn symbol into string to make the output pretty.
+        }
+      }
+  }
+  output_hash
 end
+
+#writing out what I want my code to do. Iterate through all levels, each time a value/array index matches a name, add that key to a hash
+#if the name is already a 'super key' in the new hash, just add to the hash, otherwise make a new one.
